ad6c2d2ae799d2e97b712fbdaae33d36009b4332
rocketbirdFE <housheng.wang@91sports.cc>
2017-04-07 13:58:21 +0800

    spaces change

--- config.js
+++ config.js
@@ -1,29 +1,27 @@
 var guides = [
-    { title: '通用规范', path: '/common-guide/'},
-    { title: 'Html规范', path: '/html-guide/'},
-    { title: 'Css规范', path: '/css-guide/'},
-    { title: 'JavaScript规范', path: '/javascript-guide/'},
-    { title: 'JavaScript - ESNext', path: '/es-next-guide/'}
+  { title: '通用规范', path: '/common-guide/' },
+  { title: 'Html规范', path: '/html-guide/' },
+  { title: 'Css规范', path: '/css-guide/' },
+  { title: 'JavaScript规范', path: '/javascript-guide/' },
+  { title: 'JavaScript - ESNext', path: '/es-next-guide/' }
 ]
 
 
 docute.init({
-    landing: 'landing.html',
-    debug: true,
-    nav: {
-        default: [{
-            title: '首页',
-            path: '/home/'
-        }, {
-            title: '前端规范',
-            type: 'dropdown',
-            items: guides
-        }, {
-            title: 'Vue相关约定',
-        }, ]
-    },
-    icons: [],
-    plugins: [
-        evanyou()
-    ]
+  landing: 'landing.html',
+  debug: false,
+  nav: {
+    default: [{
+      title: '首页',
+      path: '/home/'
+    }, {
+      title: '前端规范',
+      type: 'dropdown',
+      items: guides
+    }]
+  },
+  icons: [],
+  plugins: [
+    evanyou()
+  ]
 })
